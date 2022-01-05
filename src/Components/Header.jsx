f5a5e869d5457da7d2800b6e45b012d040faed67
dat.truong <truongquocdat196nt@gmail.com>
2022-01-05 21:57:55 +0700

    feat: add-button-pattern

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,10 +6,11 @@ export const Header = () => (
 			<img className='logo__item' src={Logo} alt='Logo'></img>
 		</div>
 		<div className='box-text'>
-			<div className='heading-primary'>
+			<div className='heading-primary mb-60'>
 				<div className='heading-primary__main'>Out doors</div>
 				<div className='heading-primary__sub'>is where life happens</div>
 			</div>
+			<div className='btn btn--primary'>Discover our tour</div>
 		</div>
 	</header>
 )
