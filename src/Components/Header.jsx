8b3a3f4d19dcfae6116e2d7bc8d126e2fba7342d
dat.truong <truongquocdat196nt@gmail.com>
2022-01-07 04:48:19 +0700

    feat: using-BEM

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,16 @@
 import Logo from '../assets/img/logo-white.png'
 
 export const Header = () => (
-	<header>
-		<div className='logo'>
-			<img className='logo__item' src={Logo} alt='Logo'></img>
+	<header className='header'>
+		<div className='header__logo-box'>
+			<img className='header__logo' src={Logo} alt='Logo'></img>
 		</div>
-		<div className='box-text'>
+		<div className='header__box-text'>
 			<div className='heading-primary mb-60'>
-				<div className='heading-primary__main'>Out doors</div>
-				<div className='heading-primary__sub'>is where life happens</div>
+				<div className='heading-primary--main'>Out doors</div>
+				<div className='heading-primary--sub'>is where life happens</div>
 			</div>
-			<div className='btn btn-primary btn--animation'>Discover our tour</div>
+			<div className='btn btn--primary btn--animation'>Discover our tour</div>
 		</div>
 	</header>
 )
