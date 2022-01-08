9e994618db1f9991f2d21c8a4b12a6a0a922730c
dat.truong <truongquocdat196nt@gmail.com>
2022-01-08 11:28:11 +0700

    feat: init-section-feature

---
+++ src/Components/Feature.jsx
@@ -0,0 +1,12 @@
+export const Feature = () => {
+	return (
+		<section className="section-feature">
+			<div className="row">
+				<div className="col-1-4">kjfasl</div>
+				<div className="col-1-4">slakfkl</div>
+				<div className="col-1-4"></div>
+				<div className="col-1-4"></div>
+			</div>
+		</section>
+	)
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../../Components/Header";
 import { Grid } from "../../Components/Grid";
 import { About } from "../../Components/About";
+import { Feature } from "../../Components/Feature";
 
 export const Home = () => (
 	<>
 		<Header/>
 		{/* <Grid/> */}
 		<About/>
+		<Feature/>
 	</>
 )
